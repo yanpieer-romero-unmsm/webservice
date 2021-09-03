ca948a83e0c89e1354922427e955e147995c40d2
Yanpieer Romero <yromeros@everis.com>
2021-09-03 15:19:28 -0500

    Creación de un servicio REST y llamado a una API con AJAX

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -22,6 +22,38 @@ public class UsuarioController {
         return usuario;
     }
 
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsuarios(){
+        List<Usuario> usuarios = new ArrayList<>();
+
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setNombre("Yanpieer");
+        usuario.setApellido("Romero");
+        usuario.setEmail("yanpieer13@gmail.com");
+        usuario.setTelefono("940591870");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(2L);
+        usuario2.setNombre("Marco");
+        usuario2.setApellido("Gutierrez");
+        usuario2.setEmail("marco@gmail.com");
+        usuario2.setTelefono("987654321");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(3L);
+        usuario3.setNombre("Pedro");
+        usuario3.setApellido("Larico");
+        usuario3.setEmail("pedro@gmail.com");
+        usuario3.setTelefono("965487321");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+
+        return usuarios;
+    }
+
     @RequestMapping(value = "usuario1")
     public Usuario editar(){
         Usuario usuario = new Usuario();
