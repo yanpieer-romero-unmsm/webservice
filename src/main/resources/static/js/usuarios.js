a244d2f907d5bc731e6a14281079b04deb79447a
yanpieer-romero-unmsm <yanpieer.romero@unmsm.edu.pe>
2021-09-05 00:57:32 -0500

    Se modificó el archivo para eliminar usuarios de bd

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 });
 
 async function cargarUsuarios() {
-  const request = await fetch('usuarios', {
+  const request = await fetch('api/usuarios', {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -19,14 +19,35 @@ async function cargarUsuarios() {
   let listadoHtml = '';
   for (let usuario of usuarios){
 
+      let botonEliminar = '<a href="#" onclick="eliminarUsuario(' + usuario.id + ')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>';
+
+      let telefonoTexto = usuario.telefono == null ? '-' : usuario.telefono;
+
       let usuarioHtml = '<tr><td>' + usuario.id
                           + '</td><td>' + usuario.nombre + ' ' + usuario.apellido
-                          + '</td><td>' + usuario.email + '</td><td>' + usuario.telefono
-                          + '</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
+                          + '</td><td>' + usuario.email + '</td><td>' + telefonoTexto
+                          + '</td><td>' + botonEliminar + '</td></tr>';
       listadoHtml += usuarioHtml;
 
   }
 
   document.querySelector('#usuarios tbody').outerHTML = listadoHtml;
+
+}
+
+async function eliminarUsuario(id) {
+
+  if(!confirm('¿Desea eliminar este usuario?'))
+    return;
+
+  const request = await fetch('api/usuarios/' + id, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  });
+
+  location.reload();
 
 }
