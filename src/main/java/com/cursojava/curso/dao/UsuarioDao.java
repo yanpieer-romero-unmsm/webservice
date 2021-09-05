b51381c7d2cb165b0604e9fe015ff39c05ba5b05
yanpieer-romero-unmsm <yanpieer.romero@unmsm.edu.pe>
2021-09-05 00:59:58 -0500

    Se agregó la capa DAO para relacionarse con la bd

---
+++ src/main/java/com/cursojava/curso/dao/UsuarioDao.java
@@ -0,0 +1,14 @@
+package com.cursojava.curso.dao;
+
+import com.cursojava.curso.models.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDao {
+
+    List<Usuario> getUsuarios();
+
+    void eliminar(Long id);
+
+    void registrar(Usuario usuario);
+}
---
+++ src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
@@ -0,0 +1,35 @@
+package com.cursojava.curso.dao;
+
+import com.cursojava.curso.models.Usuario;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository     //Hace referencia a la conexión con la bd, va a tener la funcionalidad de poder acceder al repositorio de bd
+@Transactional  //Da la facilidad de poder armar las consultas sql a bd, forma de como tratar y ejecutar las consultas de sql que se va a dar en fragmentos de transacción
+public class UsuarioDaoImp implements UsuarioDao{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Usuario> getUsuarios() {
+        String query = "FROM Usuario";
+        return entityManager.createQuery(query).getResultList();
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        Usuario usuario = entityManager.find(Usuario.class, id);
+        entityManager.remove(usuario);
+    }
+
+    @Override
+    public void registrar(Usuario usuario) {
+        entityManager.merge(usuario);
+    }
+
+}
