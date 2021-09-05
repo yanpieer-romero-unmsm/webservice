86b586ca392c680f07c4a1f1c2dc03700a22ca48
yanpieer-romero-unmsm <yanpieer.romero@unmsm.edu.pe>
2021-09-05 00:55:50 -0500

    Se modificó la clase para relacionarse con la bd a través de anotaciones

--- src/main/java/com/cursojava/curso/models/Usuario.java
+++ src/main/java/com/cursojava/curso/models/Usuario.java
@@ -3,15 +3,27 @@ package com.cursojava.curso.models;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column
     private String nombre;
+    @Column
     private String apellido;
+    @Column
     private String email;
+    @Column
     private String telefono;
+    @Column
     private String password;
 
 }
